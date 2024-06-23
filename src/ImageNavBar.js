2d06b71fbeeae532f39d5afce5abb0be9a97c457
Brett Deaton <brett.deaton@gmail.com>
2024-06-23 16:23:30 -0700

    Convert to flexbox

--- src/ImageNavBar.js
+++ src/ImageNavBar.js
@@ -1,7 +1,12 @@
 import "./App.css";
 
 function ImageNavBar() {
-  return <div className="image-nav-bar"></div>;
+  return (
+    <div className="image-nav-bar">
+      <button>&larr; prev</button>
+      <button>next &rarr;</button>
+    </div>
+  );
 }
 
 export default ImageNavBar;
